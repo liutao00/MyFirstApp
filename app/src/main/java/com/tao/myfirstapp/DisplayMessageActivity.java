3298a1cc199218e27eed2175e8aeae3ed82e9d0d
liutao00 <liutao00@outlook.com>
2016-06-23 00:20:16 +0800

    UI by java and xml

--- app/src/main/java/com/tao/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/tao/myfirstapp/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.tao.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -20,7 +22,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
         textView.setTextSize(40);
         textView.setText(message);
 
+
         // Set the text view as the activity layout
-        setContentView(textView);
+        setContentView(R.layout.activity_display_message);
+
+        RelativeLayout main = (RelativeLayout)findViewById(R.id.root);
+        main.addView(textView);
     }
 }
